0b29edf53e2b95fce748f4e509c421e13c764050
Andrea Canciani <ranma42@gmail.com>
2016-02-09 23:35:22 +0100

    Statically resolve vectro space methods
    and prepare for valuetype vector values

--- Integration/DiscardIntegration.fs
+++ Integration/DiscardIntegration.fs
@@ -1,111 +1,178 @@
 ﻿module DiscardIntegration
-open Swensen.Unquote.Extensions
+//open Swensen.Unquote.Extensions
 
-type FVectorSpace<'T,'U> =
-    abstract member Map : 'T -> 'U
-    abstract member Scale : float -> 'U -> 'U
-    abstract member Add : 'U -> 'U -> 'U
-    abstract member Zero : 'U
+let inline ( *=| ) (x: _ byref) s = (^U : (member Scale: float -> unit) (x, s))
+let inline ( +=| ) (x: _ byref) y = (^U : (member Add: 'U -> unit) (x, y))
 
-let floatVSpace = {
-        new FVectorSpace<bool,float> with
-        member this.Map v = if v then 1.0 else 0.0
-        member this.Scale s x = s * x
-        member this.Add a b = a + b
-        member this.Zero = 0.0
-    }
+let inline origin () = (^U : (static member Origin: unit -> ^U) ())
 
-type D<'T,'U> = FVectorSpace<'T,'U> -> 'U*float
+let inline make<'T when ^T : (new : unit -> ^T)>() = new 'T()
+
+[<StructuredFormatDisplay("F{V}")>]
+type FloatVector() =
+  [<DefaultValue>] val mutable v : float
+
+  static member inline Origin () =
+      let mutable r = make<FloatVector>()
+      r.v <- 0.0
+      r
+
+  member inline x.V = x.v
+  member inline x.Scale (s:float) = x.v <- x.v * s
+  member inline x.Add (y:FloatVector) = x.v <- x.v + y.v
+
+[<StructuredFormatDisplay("[{Map}]")>]
+type MapVector<'T when 'T : comparison>() =
+  [<DefaultValue(false)>] val mutable map : Map<'T, float>
+
+  static member Origin () =
+      let mutable r = make<MapVector<'T>>()
+      r.map <- Map.empty
+      r
+
+  member x.Map = x.map
+  member x.Scale s = x.map <- Map.map (fun k v -> v * s) x.Map
+  member x.Add (y:MapVector<'T>) =
+      let combine map k v =
+          match Map.tryFind k map with
+          | None -> map.Add(k, v)
+          | Some w -> map.Add(k, v+w)
+      x.map <- Map.fold combine x.Map y.Map
+
+
+[<StructuredFormatDisplay("{Stringed}")>]
+type DistResult<'U when ^U : (member  Scale : float -> unit)
+                    and ^U : (member    Add :    ^U -> unit)
+                    and ^U : (static member Origin :  unit -> ^U)>() =
+  [<DefaultValue(false)>] val mutable v : 'U
+  [<DefaultValue>] val mutable w : float
+
+  member inline x.Stringed = sprintf "[%A : %A]" x.v x.w
+
+  static member inline Origin () =
+      let mutable r = make<DistResult<_>>()
+      r.v <- origin ()
+      r.w <- 0.0
+      r
+
+  member inline x.Scale s =
+      &x.v *=| s
+      x.w <- x.w * s
+
+  member inline x.Add (y:DistResult<'U>) =
+      &x.v +=| y.v
+      x.w <- x.w + y.w
+
+
+type D<'T,'U  when ^U : (       member  Scale : float -> unit)
+               and ^U : (       member    Add :    ^U -> unit)
+               and ^U : (static member Origin :  unit -> ^U  )> = ('T -> 'U) -> DistResult<'U>
+
+
+let inline bool2float b =
+    let mutable r = FloatVector()
+    r.v <- if b then 1.0 else 0.0
+    r
+
+let inline zero () = origin ()
+
+let inline result v =
+    let mutable r = DistResult<_>()
+    r.v <- v
+    r.w <- 1.0
+    r
+
+let inline makeMap v =
+    let mutable r = MapVector<_>.Origin()
+    r.map <- r.map.Add(v, 1.0)
+    r
 
 type IntegratorBuilder =
     | IntegratorBuilder
-    member dist.Spiattella(x:D<'T,_>):D<'T,_> = 
-        let v,w = x {
-            new FVectorSpace<_,Map<_,float>> with
-            member self.Map v = Map.empty.Add (v,1.0)
-            member self.Scale s x = Map.map (fun k v -> s * v) x
-            member self.Add x y = x|> Map.fold (fun acc k v-> match acc.TryFind k  with None -> acc.Add(k,v)| Some w -> acc.Add(k,(v+w)) ) y
-            member self.Zero = Map.empty
-            }
-        match w with
-        | 0.0 -> fun v -> v.Zero,0.0
-        | _ ->
-            let l = v |> Map.toArray
-            fun vspace ->
-                let folder x (v,s) =
-                    vspace.Map v
-                    |>vspace.Scale s 
-                    |>vspace.Add x
-                Array.fold folder vspace.Zero l,w
+    (*
+    member inline dist.Spiattella(x:D<'T,MapVector<_>>):D<'T,_> =
+        let r = x makeMap
+        match r.snd.v with
+        | 0.0 -> fun _ -> DistResult<_> ()
+        | w ->
+            // TODO
+            let a = Map.toArray r.fst.map
+            fun f ->
+                let r = zero ()
+                reset r.fst
+                r.snd.Set(w)
+                for (v,s) in a do
+                    r.fst +=| (f v *= s)
+                r
+*)
+    member inline dist.Run x = x // dist.Spiattella x
+    member inline dist.Zero() : D<_,_> = fun f -> zero ()
+    member inline dist.Return(x) : D<_,_>  = fun f -> result (f x)
+    member inline dist.ReturnFrom(x) : D<_,_> = x
 
-    member inline dist.Run x = printf "." ; x// dist.Spiattella x
-    member inline dist.Zero() : D<_,_> =
-        fun fvspace -> (fvspace.Zero, 0.0)
-    member inline dist.Return(x) : D<_,_>  =
-        fun fvspace -> (fvspace.Map x, 1.0)
-
-    member inline dist.Bind(x:D<_,_*float>, p: _ -> D<_,_>) : D<_,_> =
-        fun fvspace ->
-        let outfvs =
-            {
-                new FVectorSpace<_,_> with
-                member self.Map v = p v fvspace
-                member self.Scale s ((x,w)) = fvspace.Scale s x,s * w
-                member self.Add ((x,wx)) ((y,wy)) = fvspace.Add x y,(wx+wy)
-                member self.Zero = fvspace.Zero, 0.0
-            }
-        match x outfvs with
-        | (_, 0.0) -> outfvs.Zero
-        | (r, w)   -> outfvs.Scale (1.0/w) r
-
-    member inline dist.cast (x:D<_,obj>): D<_,_> =
-        fun fvspace ->
-            let objvspace = {
-                new FVectorSpace<_,obj> with
-                member self.Map v = fvspace.Map v |> box
-                member self.Scale s x = fvspace.Scale s (unbox x) |> box
-                member self.Add x y = fvspace.Add (unbox x) (unbox y) |> box
-                member self.Zero = fvspace.Zero |> box
-            }
-            let (r,w) = x objvspace
-            unbox r, w
-    member inline dist.ReturnFrom(x) =
-        x
+    member inline dist.Bind(x:D<_,_>, p: _ -> D<_,_>) : D<_,_> =
+        fun f ->
+            let outf v = p v f
+            let mutable r = x outf
+            match r.w with
+            | 0.0 -> zero ()
+            | w -> &r.v *=| (1.0 / w) ; r.v
 
     [<CustomOperation("coerce", MaintainsVariableSpaceUsingBind=true)>]
-    member inline dist.Coerce(x:D<_,_>, [<ProjectionParameter>] p) : D<_,_> =
-        //let x = dist.Spiattella x
+    member inline dist.Coerce(x, [<ProjectionParameter>] p) =
         dist { let! v = x in if p v then return v }
 
 let dist = IntegratorBuilder
 
-let bernoulli x : D<_,_> =
-    fun fvspace ->
-        (fvspace.Add (fvspace.Scale x (fvspace.Map true)) (fvspace.Scale (1.0-x) (fvspace.Map false)), 1.0)
+let inline bernoulli x f =
+        let mutable ftrue = f true
+        let mutable ffalse = f false
+        &ftrue *=| x
+        &ffalse *=| (1.0 - x)
+        &ftrue +=| ffalse
+        result ftrue
 
-let uniform ((z::l)) :D<_,_> =
-    let l = List.toArray l
-    let n = 1.0 / ((Array.length l |> float) + 1.0)
-    fun fvspace ->
-        let mutable r = fvspace.Map z
-        for v in l do
-            r <- fvspace.Add r (fvspace.Map v)
-        fvspace.Scale n r,1.0
+let inline uniform l : D<_,_> =
+    match l with
+    | [] -> dist.Zero()
+    | (hd::tl) ->
+        let a = List.toArray tl
+        let s = 1.0 / (1 + Array.length a |> float)
+        fun f ->
+            let mutable r = f hd
+            for v in a do
+                &r +=| f v
+            &r *=| s                
+            result r
 
-let UnionUniform<'T,'U> () :D<'T,'U> =
-    let cases = Microsoft.FSharp.Reflection.FSharpType.GetUnionCases typeof<'T>
-    cases |> Seq.map (fun x ->  Microsoft.FSharp.Reflection.FSharpValue.MakeUnion(x,[||])) |> Seq.cast<'T>|> Seq.toList |> uniform
+let inline UnionUniform () : D<'T,_> =
+    Microsoft.FSharp.Reflection.FSharpType.GetUnionCases typeof<'T>
+    |> Array.map (fun x -> Microsoft.FSharp.Reflection.FSharpValue.MakeUnion(x, [||]) :?> _)
+    |> Array.toList
+    |> uniform
 
-let coin () = bernoulli 0.5
+let inline coin () = bernoulli 0.5
 
-let d ()=
-    dist {
-        let! a = coin () //bernoulli 0.5
-        let! b = coin () //bernoulli 0.5
-        let! c = coin () // bernoulli 0.5
+let inline myprob () =
+   dist {
+        let! a = coin ()
+        let! b = coin ()
+        let! c = coin ()
         coerce (a || b)
         coerce (c)
         return a
-    }
-let prob = d () floatVSpace
-let prob2 = coin () floatVSpace
+    } <| bool2float
+
+let eval_bernoulli x = bernoulli x bool2float
+
+printfn "%A" (eval_bernoulli 0.7)
+printfn "%A" (myprob ())
+
+let mutable v = FloatVector.Origin()
+printfn "%A" v
+v.v <- 12.0
+printfn "%A" v
+&v +=| v
+printfn "%A" v
+&v *=| 3.2
+printfn "%A" v
